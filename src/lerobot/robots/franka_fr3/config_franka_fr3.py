c54eb5c11111d6642d10657dab610d0580ab0e5f
Prabin Rath <prabinrath123@gmail.com>
2025-11-24 21:34:20 -0700

    update credits

--- src/lerobot/robots/franka_fr3/config_franka_fr3.py
+++ src/lerobot/robots/franka_fr3/config_franka_fr3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
+# ---------------------------------------------------------------------------
+# Copyright (c) 2025 Prabin Kumar Rath
+# Co-developed with Claude Sonnet 4.5 (Anthropic)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# ---------------------------------------------------------------------------
 
 from dataclasses import dataclass, field
 
--- src/lerobot/robots/franka_fr3/franka_interface.py
+++ src/lerobot/robots/franka_fr3/franka_interface.py
@@ -2,6 +2,7 @@
 
 # ---------------------------------------------------------------------------
 # Copyright (c) 2025 Prabin Kumar Rath
+# Co-developed with Claude Sonnet 4.5 (Anthropic)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
